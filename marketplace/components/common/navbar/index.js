c514b2289b49f00787d183f3cafa737357e1c66a
Tiago Pereira <tiago.pereira@ebankit.com>
2022-04-27 00:24:04 +0100

    [Marketplace] login button minor improvement

--- marketplace/components/common/navbar/index.js
+++ marketplace/components/common/navbar/index.js
@@ -6,34 +6,38 @@ export default function Navbar() {
         <section>
             <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
                 <nav className="relative" aria-label="Global">
-                <div className="flex justify-between">
-                    <div>
-                        <Link href="/"> 
-                            <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                                Home
+                    <div className="flex justify-between">
+                        <div>
+                            <Link href="/"> 
+                                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                                    Home
+                                </a>
+                            </Link>
+                        
+                            <Link href="/"> 
+                                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                                    Markertplace
+                                </a>
+                            </Link>
+                            <Link href="/"> 
+                                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                                    Blogs
+                                </a>
+                            </Link>
+                        </div>
+                        <div>
+                            <Link href="/"> 
+                                <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
+                                    Whishlist
+                                </a>
+                            </Link>
+                            <a 
+                                href="#" 
+                                className="px-8 py-3 border text-base rounded-md font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                                    Connect Wallet
                             </a>
-                        </Link>
-                    
-                        <Link href="/"> 
-                            <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                                Markertplace
-                            </a>
-                        </Link>
-                        <Link href="/"> 
-                            <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                                Blogs
-                            </a>
-                        </Link>
+                        </div>
                     </div>
-                    <div>
-                    <Link href="/"> 
-                        <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
-                            Whishlist
-                        </a>
-                    </Link>
-                    <a href="#" className="font-medium mr-8 text-indigo-600 hover:text-indigo-500">Log in</a>
-                    </div>
-                </div>
                 </nav>
             </div>
         </section>
