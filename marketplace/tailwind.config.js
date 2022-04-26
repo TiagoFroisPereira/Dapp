07ff036929efbfcd9529c798984a27626012a8ab
Tiago Pereira <tiago.pereira@ebankit.com>
2022-04-26 22:11:04 +0100

    [Marketplace] default layout

---
+++ marketplace/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false,
+  theme: {
+    extend: {
+      maxWidth: {
+        "8xl": "1920px"
+      },
+    },
+  },
+  plugins: [],
+}
