adeaca9505b4c903aeddf9add37d772c005e855a
Tiago Pereira <tiago.pereira@ebankit.com>
2022-04-27 00:17:19 +0100

    [Marketplace] More components, load courses

--- marketplace/pages/_app.js
+++ marketplace/pages/_app.js
@@ -1,7 +1,12 @@
+import { BaseLayout } from '@components/layout'
 import '@styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <BaseLayout>
+      <Component {...pageProps} />
+    </BaseLayout>
+  )
 }
 
 export default MyApp
