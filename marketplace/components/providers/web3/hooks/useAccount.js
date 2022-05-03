d55b89e765c1f95e09d05324cb42af2cfd13b89c
Tiago Pereira <tiago.pereira@ebankit.com>
2022-05-03 22:56:05 +0100

    [Improvements] added improvements and validations

--- marketplace/components/providers/web3/hooks/useAccount.js
+++ marketplace/components/providers/web3/hooks/useAccount.js
@@ -1,7 +1,35 @@
+import { useEffect } from "react"
+import useSWR from "swr"
 
+const adminAddress ={
+  "0x2e21c8589059501fa3a14c320464b771ab020bae924805ebba58aeb439740394" : true
+}
 
-export const useAccount = web3 => () => {
-  return {
-    account: web3 ? "Test Account" : "null"
+export const handler = (web3, provider) => () => {
+
+  const {data, mutate, ...rest } = useSWR(() =>
+    web3 ? "web3/accounts" : null,
+    async () => {
+      const accounts = await web3.eth.getAccounts()
+      return accounts[0]
+    }
+  )
+
+  useEffect(() => {
+    provider &&
+    provider.on("accountsChanged",
+      accounts => mutate(accounts[0] ?? null)
+    )
+  }, [provider])
+
+  return { 
+    account: {
+      data,
+      isAdmin: ( 
+        data && 
+        adminAddress[web3.utils.keccak256(data)]) ?? false,
+      mutate, 
+      ...rest
+    }
   }
 }
