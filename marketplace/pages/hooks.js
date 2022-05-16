ff6f270b8c7dc4101eacef797d7703526e047354
Tiago Pereira <tiago.pereira@ebankit.com>
2022-05-17 00:30:31 +0100

    [Marketplace] added new pages and improvements

---
+++ marketplace/pages/hooks.js
@@ -0,0 +1,35 @@
+import { useEthPrice } from "@components/hooks/useEthPrice"
+import {useState, useEffect} from "react"
+
+const useCounter = () => {
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    setInterval(() => {
+      setCount(c => c + 1)
+    }, 1000)
+  }, [])
+
+  return count
+}
+
+
+const SimpleComponent = () => {
+  // const count = useCounter()
+  const { eth } = useEthPrice()
+
+  return (
+    <h1>Simple Component - {eth.data}</h1>
+  )
+}
+
+export default function HooksPage(){
+  //const count = useCounter()
+  const { eth } = useEthPrice()
+  return(
+    <>
+      <h1>Hello Worl</h1>
+      <SimpleComponent/>
+    </>
+  )
+}
--- marketplace/pages/marketplace/index.js
+++ marketplace/pages/marketplace/index.js
@@ -1,39 +1,33 @@
 
-import { useAccount, useNetwork } from "@components/hooks/web3"
+import { useWalletInfo } from "@components/hooks/web3"
 import { Button } from "@components/ui/common"
 import { CourseCard, CourseList } from "@components/ui/course"
+import { MarketHeader } from "@components/ui/marketplace"
 import { OrderModal } from "@components/ui/order"
-import { WalletBar } from "@components/ui/web3"
 import { getAllCourses } from "@content/courses/fetcher"
 import { useState } from "react"
 
 export default function Marketplace({courses}) {
   const[selectedCourse, setSelectedCourse] = useState(null)
-  const { account } = useAccount()
-  const { network } = useNetwork()
+  const { canPurchaseCourse } = useWalletInfo()
+
   return (
     <>
     <div className="py-4">
-      <WalletBar 
-        address = {account.data}
-        network = {{
-          data: network.data,
-          target : network.target,
-          hasInitialResponse: network.hasInitialResponse,
-          isSupported: network.isSupported
-        }}
-      />
-      </div>
+      <MarketHeader/>
+    </div>
       <CourseList courses = { courses } >
         {
           (course) => 
             <CourseCard 
               key={course.id} 
               course={course}
+              disabled={!canPurchaseCourse}
               Footer={() =>
                 <div className="mt-4">
                   <Button 
                     onClick={()=> setSelectedCourse(course)}
+                    disabled={!canPurchaseCourse}
                     variant="lightPurple">
                     Purchase
                   </Button>
