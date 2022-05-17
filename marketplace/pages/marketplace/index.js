fde7013b217bfe50952fb72ce5e655ffe39a111d
Tiago Pereira <tiago.pereira@ebankit.com>
2022-05-17 22:58:49 +0100

    [Marketplace] final layout improvements

--- marketplace/pages/marketplace/index.js
+++ marketplace/pages/marketplace/index.js
@@ -11,6 +11,9 @@ export default function Marketplace({courses}) {
   const[selectedCourse, setSelectedCourse] = useState(null)
   const { canPurchaseCourse } = useWalletInfo()
 
+  const purchaseCourse = (order) =>{
+    alert(JSON.stringify(order))
+  }
   return (
     <>
     <div className="py-4">
@@ -39,6 +42,7 @@ export default function Marketplace({courses}) {
       { selectedCourse &&
         <OrderModal 
           course={selectedCourse}
+          onSubmit={purchaseCourse}
           onClose={()=> setSelectedCourse(null)}
           />
       }
